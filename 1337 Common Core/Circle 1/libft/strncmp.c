46f751d48e46955d107429aaca4cf4ce4f0572bc
root <root@DESKTOP-617DKUM.>
2024-10-26 08:22:57 +0100

    uu

--- 1337 Common Core/Circle 1/libft/strncmp.c
+++ 1337 Common Core/Circle 1/libft/strncmp.c
@@ -1,9 +1,26 @@
 #include <stdio.h>
 #include <string.h>
-int main()
-{
-	    char str1[] = "az";
-	        char str2[] = "za";
 
-		    printf("%d", strncmp(str1, str2, 2));
+int ft_strncmp(const char *s1, const char *s2, size_t n) {
+	size_t i;
+	if(n == 0) {
+		return 0;
+	}
+	i = 0;
+	while(i < n) {
+		if(s1[i] != s2[i]) {
+			return s1[i] - s2[i];
+		}
+		if(s1[i] == '\0') {
+			return 0;
+		}
+	i++;
+	}
+	return 0;
+}
+
+int main() {
+	char s1[] = "aymen";
+	char s2[] = "abmen";
+	printf("%d", ft_strncmp(s1, s2, 1));
 }
